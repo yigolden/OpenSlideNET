01969716cfe7ffa19f0a1d18b3c99cf076135e1b
yigolden <jinyi@live.cn>
2020-10-27 10:09:49 +0800

    Add SlideExporter sample.

---
+++ example/SlideExporter/DeepZoomExporter.cs
@@ -0,0 +1,72 @@
+﻿using OpenSlideNET;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace SlideExporter
+{
+    internal class DeepZoomExporter
+    {
+        public static void Run(FileInfo input, FileInfo output, int tileSize, int overlap)
+        {
+            var dziFile = new FileInfo(output.FullName + ".dzi");
+            var dziDirectory = new DirectoryInfo(output.FullName + "_files");
+
+            if (dziFile.Exists)
+            {
+                Console.WriteLine($"Error: File {dziFile.FullName} already exists.");
+                return;
+            }
+            if (dziDirectory.Exists)
+            {
+                Console.WriteLine($"Error: Directory {dziDirectory} already exists.");
+                return;
+            }
+
+            if (tileSize <= 0)
+            {
+                Console.WriteLine("Error: Tile size is invalid.");
+                return;
+            }
+            if (overlap < 0)
+            {
+                Console.WriteLine("Error: Overlap is invalid.");
+                return;
+            }
+
+            dziDirectory.Create();
+
+            using var image = OpenSlideImage.Open(input.FullName);
+            var dz = new DeepZoomGenerator(image, tileSize, overlap);
+
+            Console.WriteLine("Writing: " + dziFile.FullName);
+            File.WriteAllText(dziFile.FullName, dz.GetDzi("jpg"));
+
+            int currentCount = 0;
+            int totalCount = dz.TileCount;
+            var levelDimensions = dz.LevelDimensions.ToArray();
+            for (int level = dz.LevelCount - 1; level >= 0; level--)
+            {
+                DirectoryInfo levelDirectory = dziDirectory.CreateSubdirectory(level.ToString(CultureInfo.InvariantCulture));
+
+                long width = levelDimensions[level].Width;
+                long height = levelDimensions[level].Height;
+                int colCount = (int)((width + tileSize - 1) / tileSize);
+                int rowCount = (int)((height + tileSize - 1) / tileSize);
+
+                for (int row = 0; row < rowCount; row++)
+                {
+                    for (int col = 0; col < colCount; col++)
+                    {
+                        string path = Path.Combine(levelDirectory.FullName, $"{col}_{row}.jpg");
+                        Console.WriteLine("Writing: " + path + $" [{++currentCount}/{totalCount}]");
+                        using var fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+                        dz.GetTileAsJpegToStream(level, col, row, fs);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ example/SlideExporter/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.IO;
+
+namespace SlideExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new CommandLineBuilder();
+
+            SetupCommand(builder.Command);
+
+            builder.UseVersionOption();
+
+            builder.UseHelp();
+            builder.UseSuggestDirective();
+            builder.RegisterWithDotnetSuggest();
+            builder.UseParseErrorReporting();
+            builder.UseExceptionHandler();
+
+            Parser parser = builder.Build();
+            parser.Invoke(args);
+        }
+
+        static void SetupCommand(Command command)
+        {
+            command.Description = "Export whole slide image to other formats.";
+
+            command.AddOption(Output());
+            command.AddOption(Format());
+            command.AddOption(TileSize());
+            command.AddOption(Overlap());
+
+            command.AddArgument(new Argument<FileInfo>()
+            {
+                Name = "source",
+                Description = "The whole slide image file.",
+                Arity = ArgumentArity.ExactlyOne
+            }.ExistingOnly());
+
+            command.Handler = CommandHandler.Create<FileInfo, FileInfo, string, int, int>(Run);
+
+            static Option Output() =>
+                new Option(new[] { "--output", "--out", "-o" }, "Output location.")
+                {
+                    Name = "output",
+                    Argument = new Argument<FileInfo>() { Arity = ArgumentArity.ExactlyOne }
+                };
+
+            static Option Format() =>
+                new Option("--format", "Format. (DeepZoom)")
+                {
+                    Name = "format",
+                    Argument = new Argument<string>() { Arity = ArgumentArity.ExactlyOne }
+                };
+
+            static Option TileSize() =>
+                new Option("--tile-size", "Tile size.")
+                {
+                    Name = "tileSize",
+                    Argument = new Argument<int>(_ => 256) { Arity = ArgumentArity.ZeroOrOne }
+                };
+
+            static Option Overlap() =>
+                new Option("--overlap", "Overlap.")
+                {
+                    Name = "overlap",
+                    Argument = new Argument<int>(_ => 0) { Arity = ArgumentArity.ZeroOrOne }
+                };
+        }
+
+        private static void Run(FileInfo source, FileInfo output, string format, int tileSize, int overlap)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (output is null)
+            {
+                Console.WriteLine("Error: Output location is not specified.");
+                return;
+            }
+            if (string.IsNullOrEmpty(format))
+            {
+                Console.WriteLine("Error: Output format is not specified.");
+                return;
+            }
+
+            if (!"DeepZoom".Equals(format, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Error: Only DeepZoom format is supported.");
+                return;
+            }
+
+            DeepZoomExporter.Run(source, output, tileSize, overlap);
+        }
+    }
+}
